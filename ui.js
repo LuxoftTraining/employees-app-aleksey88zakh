0c413535e58e359d0e4f41fb7868286e8c186bfa
Алексей Захаров <azakharov@lanit.ru>
2021-11-09 11:47:37 +0600

    Labguide 2. Part 3

--- ui.js
+++ ui.js
@@ -41,10 +41,12 @@ function addEmployeeUI() {
     const name = document.getElementById("name").value;
     if (name == "") {
         errorHTML += "- Имя сотрудника должно быть задано<br>";
+        document.getElementById("name").style.backgroundColor = '#FFEEEE';
     }
     const surname = document.getElementById("surname").value;
     if (surname == "") {
         errorHTML += "- Фамилия сотрудника должна быть задана<br>";
+        document.getElementById("surname").style.backgroundColor = '#FFEEEE';
     }
     document.getElementById("addEmployeeFormErrorMessage")
         .innerHTML = errorHTML;
@@ -102,8 +104,72 @@ function getEmployeesOptions() {
     return options;
 }
 
+function searchEmployeeUI() {
+    const name = document.getElementById("nameSearch").value;
+    const surname = document.getElementById("surnameSearch").value;
+    const managerRef = document.getElementById("managerSearch").value;
+
+    const employees = searchEmployees(name, surname, managerRef);
+    showEmployees(employees);
+}
+
+function searchEmployees(name, surname, managerRef) {
+    let results = [];
+    for (let e of DATA.employees) {
+        if ((!name || e.name == name) &&
+            (!surname || e.surname == surname) &&
+            (!managerRef || e.managerRef == managerRef)) {
+            results.push(e);
+        }
+    }
+    return results;
+}
+
+/**
+ * Активирует выбранный таб
+ * @param evt событие, вызывающее активацию
+ * @param id идентификатор таба
+ */
+function openTab(evt, id) {
+    // Определяем переменные
+    var i, tabcontent, tablinks;
+
+    // Получаем все элементы с class="tabcontent" и прячем их
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Получаем все элементы с class="tablinks" и удаляем класс "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Показываем текущий таб и добавляем класс "active"
+    // на кнопку, которая открывает этот таб
+    document.getElementById(id).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function assignSendOnEnter(paneId, buttonId) {
+    let allInput = document.querySelectorAll("#" + paneId + " input");
+    for (let input of allInput) {
+        input.addEventListener("keyup", function (event) {
+            event.preventDefault();
+            if (event.keyCode === 13) {
+                document.querySelector("#" + paneId + " button").click();
+            }
+        });
+    }
+}
+
+
 function runUI() {
     showEmployees(DATA.employees);
     fillSelect(document.getElementById("managerSelect"),
         getEmployeesOptions());
+    document.getElementById("searchButton").click();
+    assignSendOnEnter("searchPane", "searchEmployeesButton");
+    assignSendOnEnter("addPane", "addEmployeeButton");
 }
